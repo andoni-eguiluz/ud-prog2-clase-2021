2866832d4fffd4de53aa6063f9b22159931b6d3f
Andoni Eguíluz Morán <andoni.eguiluz@deusto.es>
2021-02-23 12:16:39 +0100

    Añadido foreach de ejemplo

--- Clase2021/src/tema1/ejemplos/OrdenarStrings.java
+++ Clase2021/src/tema1/ejemplos/OrdenarStrings.java
@@ -8,6 +8,10 @@ public class OrdenarStrings {
 		String[] array1 = { "x", "b", "c", "hola", "bb" };  // Llamando al constructor new String[5]
 		System.out.println( java.util.Arrays.toString( array1 ) );
 		ordenar( array1 );
+		// Ejemplo de foreach para recorrer un array
+		for (String elto : array1) {
+			System.out.print( elto + " " );
+		}
 	}
 	
 	public static void ordenar( String[] array ) {
@@ -15,8 +19,10 @@ public class OrdenarStrings {
 		for (int j=0; j<array.length-1; j++) {
 			for (int i=0; i<array.length-1; i+=1) { // TODO ¿Se puede optimizar este bucle? (que haga las comparaciones necesarias, no más)
 				// System.out.println( "i = " + i );
-				if (array[i].compareTo(array[i+1]) > 0) {
+				// el1.compareTo(el2) -->   comparar el1 con el2 --> si el1< negativo, si el> positivo, si= 0
+				if (array[i].compareTo(array[i+1]) > 0) {  // array[i] > array[i+1]
 					// System.out.println( "Dos elementos mal ordenados " + i );
+					// Intercambiar array[i] con array[i+1]
 					String temp = array[i];
 					array[i] = array[i+1];
 					array[i+1] = temp;
