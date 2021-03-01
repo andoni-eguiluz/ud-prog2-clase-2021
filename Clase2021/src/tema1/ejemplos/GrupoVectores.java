dc61fbe5c0214210b491fe7f35ef94dbfcfd36b5
Andoni Eguíluz Morán <andoni.eguiluz@deusto.es>
2021-03-01 20:20:53 +0100

    Cambios en clase 1/3

--- Clase2021/src/tema1/ejemplos/GrupoVectores.java
+++ Clase2021/src/tema1/ejemplos/GrupoVectores.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class GrupoVectores {
 	private Vector2D[] arrayVects;
-	private int indiceArray; // = 0;   inic. a cero por defecto
+	private int indiceArray; // Nº de vectores guardados actualmente = 0;   inic. a cero por defecto
 
 	public GrupoVectores( int tamanyo ) {
 		arrayVects = new Vector2D[ tamanyo ];
@@ -24,6 +24,41 @@ public class GrupoVectores {
 	public void anyadir( Vector2D vec ) {
 		arrayVects[indiceArray] = vec;
 		indiceArray++;
+	}
+	
+	/** Inserta un vector en el grupo (moviendo el resto de vectores de acuerdo al nuevo).
+	 * Esta operación es errónea si el grupo ya está lleno, si el vector es nulo o si el índice está fuera de rango (menor que 0 o mayor que el size())
+	 * @param indice	Posición en la que se va a insertar
+	 * @param vec	Vector nuevo
+	 * @return	true si la inserción ha sido correcta, false si no se ha podido hacer
+	 */
+	public boolean insertar( int indice, Vector2D vec ) {
+		if (arrayVects.length==indiceArray) {
+			return false;  // Error por array lleno
+		}
+		if (vec==null) {
+			return false;  // Error por vector nulo
+		}
+		if (indice<0 || indice>indiceArray) {
+			return false;  // Error por índice de inserción fuera de rango válido
+		}
+		for (int i=size(); i>indice; i--) {
+			arrayVects[i] = arrayVects[i-1];
+		}
+		arrayVects[indice] = vec;
+		indiceArray++;
+		return true;
+	}
+	
+	/** Borra un vector del grupo
+	 * @param indice	Posición del elemento que queremos borrar
+	 */
+	public void borrar( int indice ) {
+		for (int i=indice+1; i<indiceArray; i++) {
+			arrayVects[i-1] = arrayVects[i];
+		}
+		arrayVects[indiceArray-1] = null;  // Opcional (más limpias las referencias del array)
+		indiceArray--;
 	}
 	
 	/** Comprueba que los vectores naranjas tengan la longitud correcta (en el rango [100, 250) píxels), 
@@ -46,6 +81,15 @@ public class GrupoVectores {
 			}
 		}
 		return ret;
+	}
+	
+	@Override
+	public String toString() {
+		String ret = "[";
+		for (int i=0; i<indiceArray; i++) {
+			ret += (" " + arrayVects[i].toString());
+		}
+		return ret + " ]";
 	}
 	
 }
