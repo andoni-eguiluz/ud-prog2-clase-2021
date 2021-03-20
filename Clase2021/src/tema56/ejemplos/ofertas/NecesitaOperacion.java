5c34ee3adad0b42577fd065b8fee902241a89431
Andoni Eguíluz Morán <andoni.eguiluz@deusto.es>
2021-03-20 12:04:39 +0100

    Ejemplo de ofertas con mapa y ficheros

---
+++ Clase2021/src/tema56/ejemplos/ofertas/NecesitaOperacion.java
@@ -0,0 +1,11 @@
+package tema56.ejemplos.ofertas;
+
+/** Interfaz para objetos que necesitan operación manual en su proceso
+ * @author andoni.eguiluz at ingenieria.deusto.es
+ */
+public interface NecesitaOperacion {
+	/** Devuelve el coste de la operación manual necesitada en el proceso
+	 * @return	Coste de la operación en euros
+	 */
+	public double getCosteOperacion();
+}
