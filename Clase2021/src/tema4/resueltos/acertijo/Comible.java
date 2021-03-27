5a202bfe125b897140a51903e04c6feae7721471
Andoni Eguíluz Morán <andoni.eguiluz@deusto.es>
2021-03-27 16:50:25 +0100

    Ejercicio acertijo resuelto

---
+++ Clase2021/src/tema4/resueltos/acertijo/Comible.java
@@ -0,0 +1,12 @@
+package tema4.resueltos.acertijo;
+
+/** Comportamiento de objetos que pueden ser comidos por otro objeto
+ * @author andoni.eguiluz at ingenieria.deusto.es
+ */
+public interface Comible {
+	/** Comprueba si el objeto puede ser comido por otro
+	 * @param o	objeto a comprobar
+	 * @return	true si ese objeto o puede comer al objeto en curso, false en caso contrario
+	 */
+	public boolean puedeSerComidoPor( ObjetoAcertijo o );
+}
